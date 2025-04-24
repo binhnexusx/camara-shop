de749d80688f33142da6483e4fdce3efcbc4c2e5
binhnexusx <binhhoangdev9@gmail.com>
2025-04-24 09:18:29 +0700

    f

--- src/components/States/FunctionState.js
+++ src/components/States/FunctionState.js
@@ -30,7 +30,7 @@ function FunctionState() {
                         </tr>
                     ))}
                 </tbody>
-            </table>
+            </table>    
         </div>
     );
 }
